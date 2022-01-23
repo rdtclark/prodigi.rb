75346130600b6d3b2ec739b1e4551643ecbe68cf
Robin Clark <robindtclark@gmail.com>
2022-01-23 14:42:23 +0000

    cleanup

--- lib/prodigi/object.rb
+++ lib/prodigi/object.rb
@@ -1,31 +1,6 @@
 require "ostruct"
 
-# class String
-#   def underscore
-#     self.gsub(/::/, '/').
-#       gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-#       gsub(/([a-z\d])([A-Z])/,'\1_\2').
-#       tr("-", "_").
-#       downcase
-#   end
-# end
-
 module Prodigi
-  # class Object
-  #   def initialize(attributes)
-  #     # @attributes = OpenStruct.new(attributes.transform_keys! { |key| key.to_s.underscore })
-  #     @attributes = OpenStruct.new(attributes)
-  #   end
-
-  #   def method_missing(method, *args, &block)
-  #     attribute = @attributes.send(method, *args, &block)
-  #     attribute.is_a?(Hash) ? Object.new(attribute) : attribute
-  #   end
-
-  #   def respond_to_missing?(method, include_private = false)
-  #     true
-  #   end
-  # end
   class Object < OpenStruct
     def initialize(attributes)
       super to_ostruct(attributes)
